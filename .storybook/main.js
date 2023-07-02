8e84dec4b5cf57001a09c3b53ae00bf7021fa2e9
chakri68 <chakridevireddy69@gmail.com>
2023-07-02 19:57:05 +0530

    🔧 Setup Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+/** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          implementation: require('sass'),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/web-components-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { defineCustomElements } from '../loader';
+import '../src/styles/classickit.scss';
+
+defineCustomElements();
+
+/** @type { import('@storybook/web-components').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
